1019cc528c87e5cbc7b5e6f31fa9ec9711092e17
Déborah Andrade <104578483+deborah-andrade@users.noreply.github.com>
2022-12-04 16:03:08 -0300

    Update funcao.c

--- funcao.c
+++ funcao.c
@@ -32,27 +32,3 @@ void Imprime (ContaB *conta) {
     printf("Numero: %d\n", conta->num);
     printf("Saldo: %.2f\n", conta->saldo);
 }
-
-
-
-int main (void){
-    ContaB *conta1;
-    int alea = rand();
-
-    conta1 = Cria(alea, 0.0);
-    
-    printf("Bem vindo, sua conta foi criada!!\nConfira abaixo os seus dados.\n");
-    printf("\nNumero da conta: %d\n", conta1->num);
-    printf("Seu saldo incial: %.2f\n", conta1->saldo);
-
-    
-    printf("\nApos o deposito\n");
-    Deposito(conta1, 0);
-    Imprime(conta1);
-
-    printf("\nApos o saque\n");
-    Imprime (conta1);
-    Saque(conta1, 200);
-    
-    return 0;
-}
